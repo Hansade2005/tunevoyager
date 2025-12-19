768bae68f672c89e38647832c9543ad833b97c43
Anye Happiness Ade <anye.happiness@swisslinkedu.com>
2025-12-19 20:56:21 +0100

    Add route for track details page

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/search" element={<Search />} />
+                <Route path="/track/:id" element={<TrackDetail />} />
               </Routes>
             </main>
             <MusicPlayer />
